21c4e5082d43895218b8f1e4234e1ea9e93e883b
Rodow <rodolphewojtko@gmail.com>
2019-06-07 17:47:51 +0200

    ajout des deux fenêtres modal pour la connexion et l(inscription sur l'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,25 +9,60 @@ use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Repository\RoleRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, AuthenticationUtils $authenticationUtils, RoleRepository $roleRepo)
     {
         if ($this->getUser() !== null) {
             return $this->redirectToRoute('app_home');
         }
-        return $this->render('home/index.html.twig');
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+
+            $newUser = new User();
+            $form = $this-> createForm(UserType::class, $newUser);
+            $form->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+    
+                $hash = $passwordEncoder->encodePassword($newUser, $newUser->getPassword());
+                $newUser->setPassword($hash);
+    
+                $newUser->setRole($roleRepo->findOneBy(['role' => 'user']));
+                
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($newUser);
+                $entityManager->flush();
+    
+                $this->addFlash(
+                    'success',
+                    'Votre compte à bien été créer'
+                );
+    
+                return $this->redirectToRoute('app_login');
+            }
+
+        return $this->render('home/index.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error,
+            'form' => $form->createView()
+        ]);
     }
     
     /**
      * @Route("/inscription", name="new_user")
      */
-    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder, RoleRepository $roleRepo)
     {
         if ($this->getUser() !== null) {
             return $this->redirectToRoute('app_home');
@@ -41,6 +76,8 @@ class HomeController extends AbstractController
 
             $hash = $passwordEncoder->encodePassword($newUser, $newUser->getPassword());
             $newUser->setPassword($hash);
+
+            $newUser->setRole($roleRepo->findOneBy(['role' => 'user']));
             
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($newUser);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\RoleRepository;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
